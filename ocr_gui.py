4d0df1ac0ef61ad0fc3e07bdf2212b1310135000
montinevra <23004878+montinevra@users.noreply.github.com>
2022-08-31 14:51:04 -0700

    remove magic words

--- ocr_gui.py
+++ ocr_gui.py
@@ -8,11 +8,12 @@ def ocr(files, target_dir):
 
 
 def main(argv):
+	lbl_img_select = 'Select images...'
 	file_box = sg.Listbox([], select_mode=sg.LISTBOX_SELECT_MODE_MULTIPLE, horizontal_scroll=True, key='-FILEBOX-', auto_size_text=True, expand_x=True, expand_y=True, s=(64,12))
 	target_dir = sg.Input(key='-TARGET_DIR-')
 	status_line = sg.Text('', key='-STATUS-')
 	layout = [
-			[sg.Input(key='-FILES_SELECTED-', enable_events=True, visible=False), sg.FilesBrowse('Select images...'), file_box],
+			[sg.Input(key='-FILES_SELECTED-', enable_events=True, visible=False), sg.FilesBrowse(lbl_img_select), file_box],
 			[sg.Text('Destination folder:'), target_dir, sg.FolderBrowse()],
 			[sg.Button("OCR!", button_color='green'), status_line, sg.Push(), sg.Button("Quit", button_color='red')],
 			]
@@ -23,7 +24,7 @@ def main(argv):
 		if event in (sg.WIN_CLOSED, 'Quit'):
 			break
 		elif event == '-FILES_SELECTED-':
-			file_box.update(values=vals['Select images...'].split(';'))
+			file_box.update(values=vals[lbl_img_select].split(';'))
 		elif event == 'OCR!':
 			if target_dir.get() == '':
 				status_line.update(value="Please select a destination folder", text_color='orange')
