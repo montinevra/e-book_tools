434a258b7e2fc5049f17b596f5bd4483dc7a2b63
montinevra <23004878+montinevra@users.noreply.github.com>
2022-09-06 14:08:10 -0700

    Squashed commit of the following:
    commit e34811948ea1ebded6dbf113175c5f763ce710a1
    Author: montinevra <23004878+montinevra@users.noreply.github.com>
    Date:   Fri Sep 2 16:11:30 2022 -0700
    
        add "view files" button
    
    commit 4d0df1ac0ef61ad0fc3e07bdf2212b1310135000
    Author: montinevra <23004878+montinevra@users.noreply.github.com>
    Date:   Wed Aug 31 14:51:04 2022 -0700
    
        remove magic words
    
    commit e91fc87047cd0f435505bc2ee85800402de7915f
    Author: montinevra <23004878+montinevra@users.noreply.github.com>
    Date:   Tue Aug 30 11:06:53 2022 -0700
    
        Update ocr_gui.py
    
    commit f6c5e48b4112044931becd206831facca69b6a4d
    Author: montinevra <23004878+montinevra@users.noreply.github.com>
    Date:   Thu Aug 25 17:15:35 2022 -0700
    
        add threading and destination folder
    
    commit bd6d155f064d80a1c22990f90c0c1ddb67a7443f
    Merge: e3ae03a 287861c
    Author: montinevra <23004878+montinevra@users.noreply.github.com>
    Date:   Thu Aug 25 14:19:32 2022 -0700
    
        Merge branch 'main' into gui
    
    commit e3ae03a47b1da8fd77874c99a98dc97bf5483b00
    Author: montinevra <23004878+montinevra@users.noreply.github.com>
    Date:   Thu Aug 25 14:02:02 2022 -0700
    
        Create ocr_gui.py

---
+++ ocr_gui.py
@@ -0,0 +1,60 @@
+import PySimpleGUI as sg
+import subprocess
+import os
+import sys
+
+
+def ocr(files, target_dir):
+	subprocess.call(['bash', os.path.join(os.path.dirname(__file__), 'ocr.sh')] + files, cwd=target_dir)
+
+
+def view_files(path):
+	subprocess.run(open_folder_command + [path])
+
+
+def main(argv):
+	lbl_img_select = 'Select images...'
+	file_box = sg.Listbox([], select_mode=sg.LISTBOX_SELECT_MODE_MULTIPLE, horizontal_scroll=True, key='-FILEBOX-', auto_size_text=True, expand_x=True, expand_y=True, s=(64,12))
+	target_dir = sg.Input(key='-TARGET_DIR-')
+	status_line = sg.Text('', key='-STATUS-')
+	layout = [
+			[sg.Input(key='-FILES_SELECTED-', enable_events=True, visible=False), sg.FilesBrowse(lbl_img_select), file_box],
+			[sg.Text('Destination folder:'), target_dir, sg.FolderBrowse()],
+			[sg.Button("OCR!", button_color='green'), status_line, sg.pin(sg.Button('View Files', key='-VIEW_FILES-', visible=False)), sg.Push(), sg.Button("Quit", button_color='red')],
+			# [status_line, sg.Button('View Files', key="-VIEW_FILES-", visible=False)],
+			]
+	window = sg.Window('OCR', layout,  resizable=True, font=('', 16))
+
+	while True:
+		event, vals = window.read()
+		if event in (sg.WIN_CLOSED, 'Quit'):
+			break
+		elif event == '-FILES_SELECTED-':
+			file_box.update(values=vals[lbl_img_select].split(';'))
+		elif event == 'OCR!':
+			if target_dir.get() == '':
+				status_line.update(value="Please select a destination folder", text_color='orange')
+				continue
+			target = target_dir.get()
+			window.perform_long_operation(lambda: ocr(file_box.get_list_values(), target), '-OCR_FINISHED-')
+			status_line.update(value="OCR-ing!", text_color=None)
+			window['-VIEW_FILES-'].update(visible=False)
+		elif event == '-OCR_FINISHED-':
+			status_line.update(value="OCR finished!", text_color='light green')
+			# window["-VIEW_FILES-"].update(visible=False)
+			window['-VIEW_FILES-'].update(visible=True)
+		elif event == '-VIEW_FILES-':
+			view_files(target)
+	window.close()
+
+
+open_folder_commands = {
+	'darwin': ['open', '--'],
+	'linux2': ['xdg-open', '--'],
+	'win32': ['explorer'],
+}
+open_folder_command = open_folder_commands[sys.platform]
+
+if __name__ == "__main__":
+
+	main(sys.argv)
