729a5cfc29cad403ca028beff587e21e6b06f5f3
montinevra <23004878+montinevra@users.noreply.github.com>
2022-10-10 14:19:41 -0700

    add LCID setting

--- xhtml_from_hocr.py
+++ xhtml_from_hocr.py
@@ -68,7 +68,7 @@ def print_out(args):
 	parser = HtmlFromHocr()
 	print(
 		"<?xml version='1.0' encoding='utf-8'?>\n" + 
-		'<html xmlns="http://www.w3.org/1999/xhtml" xmlns:epub="http://www.idpf.org/2007/ops" lang="en" xml:lang="en">\n' +
+		f'<html xmlns="http://www.w3.org/1999/xhtml" xmlns:epub="http://www.idpf.org/2007/ops" lang="{args.language}" xml:lang="{args.language}">\n' +
 		"<head>\n" + 
 		f" <title>{args.title}</title>\n" +
 		"</head>\n\n" +
@@ -95,6 +95,7 @@ if __name__ == "__main__":
 	argparser = argparse.ArgumentParser()
 	argparser.add_argument("file", nargs="*", type=argparse.FileType('r'), help="File(s) to convert.")
 	argparser.add_argument("-F", "--foffset", type=int, default=0, help="Same as -O, but for front matter.")
+	argparser.add_argument("-l", "--language", type=str, default="en", help="Set the LCID for the document. Defaults to 'en'")
 	argparser.add_argument("-O", "--offset", type=int, default=0, help="Offset the page number of main body matter by this amount. Useful if the filename does not match the page number. For example, if page001.jpg contains page 3, set this to 2. Can be nagative.")
 	argparser.add_argument("-o", "--output", type=argparse.FileType('w'), help="Output file. Defaults to stdout.")
 	argparser.add_argument("-p", "--prefix", type=str, default="page", help="Specify the filename prefix. Defaults to 'page'. Filenames with this prefix are considered main body matarial. All other files are considered front material and will be numbered using roman numerals.")
