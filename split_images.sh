d3de783b66c219ad045d0e33a47a219f441b8895
montinevra <23004878+montinevra@users.noreply.github.com>
2023-09-07 15:41:47 -0700

    Update split_images.sh

--- split_images.sh
+++ split_images.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 #
+# input_file="your_input_file.jpg"
+# prefix=$(echo "$input_file" | sed -E 's/(.+)\..+/\1/')
+# output_file="${prefix}%03d.${input_file##*.}"
+#
+# convert "$input_file" -crop 50%x100% +repage "$output_file"
+
 #  mogrify -rotate 270 *.jpg
 convert page*.jpg -crop 50%x100% page%03d.jpg
